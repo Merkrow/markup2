8b85f3b7e449d4ef0ab12de2ea2e3b2b7d98d628
Sergey Pliev <merkrow@gmail.com>
2016-11-02 16:55:05 +0200

    no message

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	
 $('.main-item').click(function () {
     $(this).next().slideToggle();
+    $(this).children('li').toggleClass('actingwhite actingblack');
+    $(this).parent('ul.listopen, ul.listclose').toggleClass('listopen listclose');
     if($(this).children('li').hasClass('arrow') || $(this).children('li').hasClass('arrow-down')) {
     $(this).children('li').toggleClass('arrow arrow-down');
     }
